d8e9dc66d3087f631c6aecec3a444fb8a34b0fcc
Ross Smith II <ross@smithii.com>
2025-05-08 21:36:20 -0700

    chore: remove main_test

--- cmd/main.go
+++ cmd/main.go
@@ -10,22 +10,16 @@ import (
 	"log"
 	"os"
 	"path/filepath"
-	"strconv"
 
 	"github.com/rasa/compat"
 )
 
 func main() {
-	fmt.Printf("sameDevice() returned  %v\n", sameDevice())
-	fmt.Printf("sameDevicel() returned %v\n", sameDevicel())
-	fmt.Printf("sameDevices() returned %v\n", sameDevices())
-	fmt.Printf("sameFile() returned    %v\n", sameFile())
-	fmt.Printf("sameFilel() returned   %v\n", sameFilel())
-	fmt.Printf("sameFiles() returned   %v\n", sameFiles())
 	exe, _ := os.Executable()
 	fi, err := compat.Stat(exe)
 	if err != nil {
-		log.Fatal(err)
+		log.Print(err)
+		return
 	}
 	fmt.Printf("Name()    =%v\n", fi.Name())
 	fmt.Printf("Size()    =%v\n", fi.Size())
@@ -42,9 +36,16 @@ func main() {
 	fmt.Printf("MTime()   =%v\n", fi.MTime())
 	fmt.Printf("UID()     =%v\n", fi.UID())
 	fmt.Printf("GID()     =%v\n", fi.GID())
+
+	sameDevice()
+	sameDevicel()
+	sameDevices()
+	sameFile()
+	sameFilel()
+	sameFiles()
 }
 
-func sameDevice() string {
+func sameDevice() {
 	exe, _ := os.Executable()
 	fi1, err := compat.Stat(exe)
 	if err != nil {
@@ -55,33 +56,33 @@ func sameDevice() string {
 		log.Fatal(err)
 	}
 
-	return strconv.FormatBool(compat.SameDevice(fi1, fi2))
+	fmt.Printf("SameDevice(): %v\n", compat.SameDevice(fi1, fi2))
 }
 
-func sameDevicel() string {
+func sameDevicel() {
 	exe, _ := os.Executable()
 	link := "link" + filepath.Ext(exe)
 	_ = os.Link(exe, link)
 	defer os.Remove(link)
 	fi1, err := compat.Lstat(link)
 	if err != nil {
-		log.Fatal(err)
+		log.Fatal(err) //nolint:gocritic // quiet linter
 	}
 	fi2, err := compat.Lstat(link)
 	if err != nil {
 		log.Fatal(err)
 	}
 
-	return strconv.FormatBool(compat.SameDevice(fi1, fi2))
+	fmt.Printf("SameDevicel(): %v\n", compat.SameDevice(fi1, fi2))
 }
 
-func sameDevices() string {
+func sameDevices() {
 	exe, _ := os.Executable()
 
-	return strconv.FormatBool(compat.SameDevices(exe, exe))
+	fmt.Printf("SameDevices(): %v\n", compat.SameDevices(exe, exe))
 }
 
-func sameFile() string {
+func sameFile() {
 	exe, _ := os.Executable()
 	fi1, err := compat.Stat(exe)
 	if err != nil {
@@ -92,28 +93,28 @@ func sameFile() string {
 		log.Fatal(err)
 	}
 
-	return strconv.FormatBool(compat.SameFile(fi1, fi2))
+	fmt.Printf("SameFile(): %v\n", compat.SameFile(fi1, fi2))
 }
 
-func sameFilel() string {
+func sameFilel() {
 	exe, _ := os.Executable()
 	link := "link" + filepath.Ext(exe)
 	_ = os.Link(exe, link)
 	defer os.Remove(link)
 	fi1, err := compat.Lstat(link)
 	if err != nil {
-		log.Fatal(err)
+		log.Fatal(err) //nolint:gocritic // quiet linter
 	}
 	fi2, err := compat.Lstat(link)
 	if err != nil {
 		log.Fatal(err)
 	}
 
-	return strconv.FormatBool(compat.SameFile(fi1, fi2))
+	fmt.Printf("SameFilel(): %v\n", compat.SameFile(fi1, fi2))
 }
 
-func sameFiles() string {
+func sameFiles() {
 	exe, _ := os.Executable()
 
-	return strconv.FormatBool(compat.SameFiles(exe, exe))
+	fmt.Printf("SameFiles(): %v\n", compat.SameFiles(exe, exe))
 }
