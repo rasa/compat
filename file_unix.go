94b9650eb9e57e2b39aa83d68235633de6f4e716
Ross Smith II <ross@smithii.com>
2025-05-08 22:55:25 -0700

    chore: add illumos support

--- file_unix.go
+++ file_unix.go
@@ -1,7 +1,7 @@
 // SPDX-FileCopyrightText: Copyright © 2025 Ross Smith II <ross@smithii.com>
 // SPDX-License-Identifier: MIT
 
-//go:build aix || android || dragonfly || linux || openbsd || solaris
+//go:build aix || android || dragonfly || illumos || linux || openbsd || solaris
 
 package compat
 
