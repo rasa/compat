0ec9d996e26a5dee2e41f6eef146cb995a6262b6
Ross Smith II <ross@smithii.com>
2025-05-09 15:42:06 -0700

    feat: add admin functions

---
+++ admin_windows.go
@@ -0,0 +1,44 @@
+// SPDX-FileCopyrightText: Copyright © 2025 Ross Smith II <ross@smithii.com>
+// SPDX-License-Identifier: MIT
+
+//go:build windows
+
+package compat
+
+import (
+	"golang.org/x/sys/windows"
+)
+
+// IsAdmin returns true if the user is root, or has Windows Administrator rights.
+func IsAdmin() (bool, error) {
+	// Source: https://github.com/golang/go/issues/28804#issuecomment-505326268
+	var sid *windows.SID
+
+	// Although this looks scary, it is directly copied from the
+	// official windows documentation. The Go API for this is a
+	// direct wrap around the official C++ API.
+	// See https://docs.microsoft.com/en-us/windows/desktop/api/securitybaseapi/nf-securitybaseapi-checktokenmembership
+	err := windows.AllocateAndInitializeSid(
+		&windows.SECURITY_NT_AUTHORITY,
+		2,
+		windows.SECURITY_BUILTIN_DOMAIN_RID,
+		windows.DOMAIN_ALIAS_RID_ADMINS,
+		0, 0, 0, 0, 0, 0,
+		&sid)
+	if err != nil {
+		return false, err
+	}
+	defer windows.FreeSid(sid)
+
+	// This appears to cast a null pointer so I'm not sure why this
+	// works, but this guy says it does and it Works for Me™:
+	// https://github.com/golang/go/issues/28804#issuecomment-438838144
+	token := windows.Token(0)
+
+	isMember, err := token.IsMember(sid)
+	if err != nil {
+		return false, err
+	}
+
+	return isMember, nil
+}
