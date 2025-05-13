125171a673fa9c8304aeb835c7ac73eba98cb25a
Ross Smith II <ross@smithii.com>
2025-05-13 10:59:42 -0700

    fix(docs): add package documentation in doc.go

---
+++ doc.go
@@ -0,0 +1,8 @@
+// SPDX-FileCopyrightText: Copyright © 2025 Ross Smith II <ross@smithii.com>
+// SPDX-License-Identifier: MIT
+
+// package compat is a pure-Go library providing unified access to file and
+// device metadata, atomic file operations, process priority, etc. on all major
+// operating systems, include Windows, Linux, macOS, Android, iOS, and many
+// others.
+package compat
