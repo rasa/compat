f9277f30ce2421d4505f5a80212aa63f00a37d88
Ross Smith II <ross@smithii.com>
2025-05-27 07:52:15 -0700

    feat(test): add bits/runtime tests

---
+++ runtime_test.go
@@ -0,0 +1,91 @@
+// SPDX-FileCopyrightText: Copyright © 2025 Ross Smith II <ross@smithii.com>
+// SPDX-License-Identifier: MIT
+
+package compat_test
+
+import (
+	"bytes"
+	"os/exec"
+	"slices"
+	"strings"
+	"testing"
+)
+
+var oses = []string{
+	"aix",
+	"android",
+	"darwin",
+	"dragonfly",
+	"freebsd",
+	"illumos",
+	"ios",
+	"js",
+	"linux",
+	"netbsd",
+	"openbsd",
+	"plan9",
+	"solaris",
+	"wasip1",
+	"windows",
+}
+
+var arches = []string{
+	"386",
+	"amd64",
+	"arm",
+	"arm64",
+	"loong64",
+	"mips",
+	"mips64",
+	"mips64le",
+	"mipsle",
+	"ppc64",
+	"ppc64le",
+	"riscv64",
+	"s390x",
+	"wasm",
+}
+
+func TestRuntime(t *testing.T) {
+	out, err := exec.Command("go", "tool", "dist", "list").Output()
+	if err != nil {
+		t.Fatal(err)
+	}
+
+	gooses := make(map[string]struct{})
+	goarches := make(map[string]struct{})
+
+	lines := bytes.Split(out, []byte{'\n'})
+	for _, line := range lines {
+		before, after, found := strings.Cut(string(line), "/")
+		if !found {
+			continue
+		}
+		gooses[before] = struct{}{}
+		goarches[after] = struct{}{}
+	}
+
+	for goos := range gooses {
+		if !slices.Contains(oses, goos) {
+			t.Errorf("found new GOOS: %q", goos)
+		}
+	}
+	for goarch := range goarches {
+		if !slices.Contains(arches, goarch) {
+			t.Errorf("found new GOARCH: %q", goarch)
+		}
+	}
+
+	for _, goos := range oses {
+		_, ok := gooses[goos]
+		if !ok {
+			t.Logf("go no longer supports GOOS: %q", goos)
+		}
+	}
+	for _, goarch := range arches {
+		_, ok := goarches[goarch]
+		if !ok {
+			t.Logf("go no longer supports GOARCH: %q", goarch)
+		}
+	}
+}
