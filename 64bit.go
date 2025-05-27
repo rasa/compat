a494187d008f4a8fb91825e3fd8830590d7e9be3
Ross Smith II <ross@smithii.com>
2025-05-26 20:56:19 -0600

    feat: add 64bit.go

---
+++ 64bit.go
@@ -0,0 +1,16 @@
+// SPDX-FileCopyrightText: Copyright © 2025 Ross Smith II <ross@smithii.com>
+// SPDX-License-Identifier: MIT
+
+//go:build !(386 || arm || mips || mipsle)
+
+package compat
+
+// Is32bit reports whether the build target is 32bit.
+func Is32bit() bool {
+  return false
+}
+
+// Is64bit reports whether the build target is 64bit.
+func Is64bit() bool {
+  return true
+}
