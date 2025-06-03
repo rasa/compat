5c9782e58d46d7e51b3445db9926d61d68e9c9f2
Ross Smith II <ross@smithii.com>
2025-06-03 14:17:23 -0700

    fix(build): fix lint issue

--- file_debug_windows.go
+++ file_debug_windows.go
@@ -1,7 +1,7 @@
 // SPDX-FileCopyrightText: Copyright © 2025 Ross Smith II <ross@smithii.com>
 // SPDX-License-Identifier: MIT
 
-//go:build windows && debug
+//go:build windows && debug && ignore
 
 package compat
 
@@ -16,7 +16,7 @@ import (
 	"golang.org/x/sys/windows"
 )
 
-func dumpMasks(perm os.FileMode, ownerMask uint32, groupMask uint32, worldMask uint32) {
+func dumpMasks(perm os.FileMode, ownerMask uint32, groupMask uint32, worldMask uint32) { //nolint:unused // quiet linter
 	if !testing.Verbose() {
 		return
 	}
@@ -31,7 +31,7 @@ func dumpMasks(perm os.FileMode, ownerMask uint32, groupMask uint32, worldMask u
 }
 
 // https://github.com/golang/sys/blob/3d9a6b80/windows/security_windows.go#L992
-var maskMap = map[uint32]string{
+var maskMap = map[uint32]string{ //nolint:unused // quiet linter
 	windows.GENERIC_READ:    "GR", // 0x80000000
 	windows.GENERIC_WRITE:   "GW", // 0x40000000
 	windows.GENERIC_EXECUTE: "GE", // 0x20000000
@@ -39,9 +39,9 @@ var maskMap = map[uint32]string{
 	windows.DELETE:          "D",  // 0x00010000
 }
 
-type aMask uint32
+type aMask uint32 //nolint:unused // quiet linter
 
-func (a aMask) String() string {
+func (a aMask) String() string { //nolint:unused // quiet linter
 	mask := uint32(a)
 	rv := ""
 	rights := map[string]uint32{}
