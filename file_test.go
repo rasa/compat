e80bc94001bfaeb4d55bcb1b505feb53da599d0b
Ross Smith II <ross@smithii.com>
2025-07-26 13:26:47 -0700

    Update file_test.go

--- file_test.go
+++ file_test.go
@@ -309,7 +309,7 @@ func TestFilePosixOpenFileDelete(t *testing.T) {
 	if err != nil {
 		// workaround:
 		// https://github.com/rasa/compat/actions/runs/16542086538/job/46784707170#step:6:48
-		if compat.IsDarwin {
+		if compat.IsDarwin || compat.IsIos {
 			t.Skip(err)
 		} else {
 			t.Fatal(err)
