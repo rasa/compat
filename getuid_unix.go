23c56bb367dadd45b2e78434062c8295e87fbeae
Ross Smith II <ross@smithii.com>
2025-08-10 20:28:14 -0700

    fix: quiet linter re getuid_unix.go

--- getuid_unix.go
+++ getuid_unix.go
@@ -15,12 +15,12 @@ import (
 // converted to it's POSIX equivalent, which is compatiable with Cygwin and
 // Git for Windows. On Plan9, the User ID is a 64-bit hash of the user's name.
 func Getuid() (uint64, error) {
-	return uint64(syscall.Getuid()), nil
+	return uint64(syscall.Getuid()), nil //nolint:gosec // quiet linter
 }
 
 // Getgid returns the Group ID as a uint64. On Windows, the user's primary group's
 // SID is converted to its POSIX equivalent, which is compatiable with Cygwin and
 // Git for Windows. On Plan9, the Getgid returns the value returned by Getuid().
 func Getgid() (uint64, error) {
-	return uint64(syscall.Getgid()), nil
+	return uint64(syscall.Getgid()), nil //nolint:gosec // quiet linter
 }
