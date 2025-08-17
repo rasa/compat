0cc352dbfb34d7cb3f47d5a928567234737ec79b
Ross Smith II <ross@smithii.com>
2025-08-16 20:07:24 -0700

    fix: quiet linter re format.go

--- format.go
+++ format.go
@@ -13,7 +13,7 @@ package compat
 //	- hello.go
 func FormatDirEntry(dir DirEntry) string {
 	name := dir.Name()
-	b := make([]byte, 0, 5+len(name))
+	b := make([]byte, 0, 5+len(name)) //nolint:mnd // quiet linter
 
 	// The Type method does not return any permission bits,
 	// so strip them from the string.
