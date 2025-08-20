b4b6d4e3df2d8f46a5b484b387d41a2614ac5448
Ross Smith II <ross@smithii.com>
2025-08-20 08:20:48 -0700

    fix: comments in shim_windows.go

--- golang/shim_windows.go
+++ golang/shim_windows.go
@@ -15,9 +15,9 @@ import (
 	"golang.org/x/sys/windows"
 )
 
-// Redefining here to avoid a circular dependency.
-// O_DELETE deletes the file when closed.
 const (
+	// Redefining here to avoid a circular dependency.
+	// O_DELETE deletes the file when closed.
 	O_DELETE = 0x8000000
 	// O_NOROATTR doesn't set a file's read-only attribute if mode
 	O_NOROATTR = 0x4000000
