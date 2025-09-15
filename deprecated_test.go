f62011fc91a0d3dae2928cecf6edfbe0114af1a7
Ross Smith II <ross@smithii.com>
2025-09-15 12:36:46 -0700

    fix: add deprecated_test.go

---
+++ deprecated_test.go
@@ -0,0 +1,14 @@
+// SPDX-FileCopyrightText: Copyright © 2025 Ross Smith II <ross@smithii.com>
+// SPDX-License-Identifier: MIT
+
+package compat_test
+
+import (
+	"testing"
+
+	"github.com/rasa/compat"
+)
+
+func TestDeprecatedSupported(t *testing.T) {
+	_ = compat.Supported(compat.Links)
+}
