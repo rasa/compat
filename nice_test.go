35740fe77f9e3de1f956e1fee5f19d63c40bf81f
Ross Smith II <ross@smithii.com>
2025-09-17 05:57:36 -0700

    fix: check compatibility after executing Nice /Renice

--- nice_test.go
+++ nice_test.go
@@ -11,39 +11,33 @@ import (
 )
 
 func TestNice(t *testing.T) {
-	if !compat.SupportsNice() {
-		skipf(t, "Skipping test: Nice() is not supported on %v/%v", runtime.GOOS, runtime.GOARCH)
-
-		return // tinygo doesn't support t.Skip
-	}
-
 	_, err := compat.Nice()
 	if err != nil {
+		if !compat.SupportsNice() {
+			skipf(t, "Skipping test: Nice() is not supported on %v/%v", runtime.GOOS, runtime.GOARCH)
+
+			return // tinygo doesn't support t.Skip
+		}
+
 		t.Fatal(err)
 	}
 }
 
 func TestNiceRenice(t *testing.T) {
-	if !compat.SupportsNice() {
-		skipf(t, "Skipping test: Nice() is not supported on %v/%v", runtime.GOOS, runtime.GOARCH)
-
-		return // tinygo doesn't support t.Skip
-	}
-
 	err := compat.Renice(compat.MaxNice)
 	if err != nil {
+		if !compat.SupportsNice() {
+			skipf(t, "Skipping test: Renice() is not supported on %v/%v", runtime.GOOS, runtime.GOARCH)
+
+			return // tinygo doesn't support t.Skip
+		}
+
 		// Don't fail on "permission denied" on Linux
 		skip(t, err)
 	}
 }
 
 func TestNiceReniceIfRootValid(t *testing.T) {
-	if !compat.SupportsNice() {
-		skipf(t, "Skipping test: Nice() is not supported on %v/%v", runtime.GOOS, runtime.GOARCH)
-
-		return // tinygo doesn't support t.Skip
-	}
-
 	isRoot, _ := compat.IsRoot()
 
 	if !compat.IsWindows && !isRoot {
@@ -54,12 +48,24 @@ func TestNiceReniceIfRootValid(t *testing.T) {
 
 	nice, err := compat.Nice()
 	if err != nil {
+		if !compat.SupportsNice() {
+			skipf(t, "Skipping test: Nice() is not supported on %v/%v", runtime.GOOS, runtime.GOARCH)
+
+			return // tinygo doesn't support t.Skip
+		}
+
 		t.Fatal(err)
 	}
 
 	for n := 0; n >= compat.MinNice; n-- {
 		err = compat.Renice(n)
 		if err != nil {
+			if !compat.SupportsNice() {
+				skipf(t, "Skipping test: Renice() is not supported on %v/%v", runtime.GOOS, runtime.GOARCH)
+
+				return // tinygo doesn't support t.Skip
+			}
+
 			// under act, "permission denied" is returned, even though we root.
 			skip(t, err)
 
@@ -74,12 +80,6 @@ func TestNiceReniceIfRootValid(t *testing.T) {
 }
 
 func TestNiceReniceIfRootInvalid(t *testing.T) {
-	if !compat.SupportsNice() {
-		skipf(t, "Skipping test: Nice() is not supported on %v/%v", runtime.GOOS, runtime.GOARCH)
-
-		return // tinygo doesn't support t.Skip
-	}
-
 	isRoot, _ := compat.IsRoot()
 
 	if !compat.IsWindows && !isRoot {
@@ -92,8 +92,16 @@ func TestNiceReniceIfRootInvalid(t *testing.T) {
 
 	err := compat.Renice(invalidNice)
 	if err == nil {
+		if !compat.SupportsNice() {
+			skipf(t, "Skipping test: Nice() is not supported on %v/%v", runtime.GOOS, runtime.GOARCH)
+
+			return // tinygo doesn't support t.Skip
+		}
+
 		if !compat.IsWindows && !compat.IsPlan9 {
 			skipf(t, "got no error calling Renice with %v (ignoring: doesn't fail on %v)", invalidNice, runtime.GOOS)
+
+			return
 		}
 		fatalf(t, "got no error calling Renice with %v", invalidNice)
 
