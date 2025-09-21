2d9d4ec0b265173d5beb0b463706626822acf42f
Ross Smith II <ross@smithii.com>
2025-09-20 17:35:30 -0700

    fix: change go test command to remove verbosity
    Updated test command to simplify execution.

--- download-build-test.sh
+++ download-build-test.sh
@@ -25,7 +25,9 @@ printf 'GOARCH: %s\n' "${GOARCH}"
 tmp1=$(mktemp)
 curl -L -s -o "${tmp1}" 'https://go.dev/dl/?mode=json'
 jqcmd="[ .[] | select(.stable == true) ][0] | .files[] | select(.os == \"${GOOS}\" and .arch == \"${GOARCH}\")"
+
 unset GOOS GOARCH
+
 name=$(jq -r "${jqcmd} | .filename" "${tmp1}")
 printf 'name:   %s\n' "${name}"
 hash=$(jq -r "${jqcmd} | .sha256" "${tmp1}")
@@ -34,15 +36,22 @@ size=$(jq -r "${jqcmd} | .size" "${tmp1}")
 printf 'size:   %s\n' "${size}"
 base=$(basename "${name}" .tar.gz)
 printf 'base:   %s\n' "${base}"
+
 mkdir -p "../${base}"
 cd "../${base}" || exit
+
 printf 'Downloading %s...\n' "https://go.dev/dl/${name}"
 curl -L -s -o "${name}" "https://go.dev/dl/${name}"
+
 printf '%s %s\n' "${hash}" "${name}" | sha256sum -c
+
 printf 'Untarring %s to %s...\n' "${name}" "${PWD}"
 gtar xzf "${name}"
+
 rm -f "${name}" "${tmp1}"
+
 export PATH="${PWD}/go/bin:${PATH}"
+
 cd "${GITHUB_WORKSPACE}" || exit
 
 GOVERSION=$(go version || true)
@@ -58,7 +67,7 @@ else
   printf '::error ::build failed: %s (error %s)\n' "${GOVERSION}" "${rv}"
 fi
 
-if go test -v .; then
+if go test .; then
   printf '::notice ::tests succeeded: %s\n' "${GOVERSION}"
 else
   rv=$?
