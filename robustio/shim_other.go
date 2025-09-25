8c361e30e09477d97085bd637338a88070f34f88
Ross Smith II <ross@smithii.com>
2025-09-24 20:23:24 -0700

    fix: build on linux

--- robustio/shim_other.go
+++ robustio/shim_other.go
@@ -8,5 +8,6 @@ package robustio
 // retry retries ephemeral errors from f up to an arbitrary timeout
 // to work around filesystem flakiness on Windows and Darwin.
 func retry(f func() (_ error, _ bool), _ float64) error {
-	return f
+	err, _ := f()
+	return err
 }
