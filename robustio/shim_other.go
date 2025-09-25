8ab71ad5f377c0bcf4422c8b9943442242d22a9d
Ross Smith II <ross@smithii.com>
2025-09-24 19:52:03 -0700

    fix: build with robustio/shim_other.go

---
+++ robustio/shim_other.go
@@ -0,0 +1,12 @@
+// SPDX-FileCopyrightText: Copyright © 2025 Ross Smith II <ross@smithii.com>
+// SPDX-License-Identifier: MIT
+
+//go:build !windows && !darwin
+
+package robustio
+
+// retry retries ephemeral errors from f up to an arbitrary timeout
+// to work around filesystem flakiness on Windows and Darwin.
+func retry(f func() (_ error, _ bool), _ float64) error {
+	return f
+}
