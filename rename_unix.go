18746087bdbf9f5db4d0adfae48bae8d5f79caab
Ross Smith II <ross@smithii.com>
2025-09-24 19:31:30 -0700

    fix: linux build

--- rename_unix.go
+++ rename_unix.go
@@ -1,9 +1,6 @@
 // SPDX-FileCopyrightText: Copyright © 2025 Ross Smith II <ross@smithii.com>
 // SPDX-License-Identifier: MIT
 
-// SPDX-FileCopyrightText: Portions copyright (c) 2015 Nate Finch (@natefinch)
-// SPDX-FileCopyrightText: Portions copyright (c) 2022 Simon Dassow (@sdassow)
-
 //go:build !windows
 
 package compat
@@ -12,9 +9,9 @@ import (
 	"os"
 )
 
-// Rename atomically replaces the destination file or directory with the
+// rename atomically replaces the destination file or directory with the
 // source. It is guaranteed to either replace the target file entirely, or not
 // change either file.
-func Rename(source, destination string, _ ...Option) error {
+func rename(source, destination string, _ ...Option) error {
 	return os.Rename(source, destination)
 }
