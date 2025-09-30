6f736e1b8d0530b84ea323d5da80899540790451
Ross Smith II <ross@smithii.com>
2025-09-30 09:18:15 -0700

    fix: add atomic file writing tests
    Added tests for atomic file writing using WriteFileAtomic and WriteReaderAtomic functions.

--- deprecated_test.go
+++ deprecated_test.go
@@ -1,4 +1,66 @@
 // SPDX-FileCopyrightText: Copyright © 2025 Ross Smith II <ross@smithii.com>
 // SPDX-License-Identifier: MIT
 
+// SPDX-FileCopyrightText: Portions copyright (c) 2015 Nate Finch (@natefinch)
+// SPDX-FileCopyrightText: Portions copyright (c) 2022 Simon Dassow (@sdassow)
+
 package compat_test
+
+import (
+	"os"
+	"testing"
+
+	"github.com/rasa/compat"
+)
+
+func TestWriteFileAtomic(t *testing.T) { //nolint:dupl
+	file, err := tempName(t)
+	if err != nil {
+		t.Fatalf("Failed to create temp file: %v", err)
+	}
+
+	cleanup(t, file)
+
+	perm := compat.CreatePerm // 0o666
+	err = compat.WriteFileAtomic(file, helloBytes, perm)
+	if err != nil {
+		t.Fatalf("Failed to write file: %q: %v", file, err)
+	}
+
+	fi, err := compat.Stat(file)
+	if err != nil {
+		t.Fatalf("Failed to stat file: %q: %v", file, err)
+	}
+
+	want := fixPerms(perm, false)
+	got := fi.Mode().Perm()
+	if got != want {
+		t.Fatalf("got %04o, want %04o", got, want)
+	}
+}
+
+func TestWriteReaderAtomic(t *testing.T) { //nolint:dupl
+	file, err := tempName(t)
+	if err != nil {
+		t.Fatalf("Failed to create temp file: %v", err)
+	}
+
+	cleanup(t, file)
+
+	perm := compat.CreatePerm // 0o666
+	err = compat.WriteReaderAtomic(file, helloBuf, perm)
+	if err != nil {
+		t.Fatalf("Failed to write file: %q: %v", file, err)
+	}
+
+	fi, err := compat.Stat(file)
+	if err != nil {
+		t.Fatalf("Failed to stat file: %q: %v", file, err)
+	}
+
+	want := fixPerms(perm, false)
+	got := fi.Mode().Perm()
+	if got != want {
+		t.Fatalf("got %04o, want %04o", got, want)
+	}
+}
