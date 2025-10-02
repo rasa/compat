905de99fe9c6a322b3b612409e8533ef8e8f9a68
Ross Smith II <ross@smithii.com>
2025-10-01 20:06:23 -0700

    chore: add robustio/urls.txt & readme

--- robustio/robustio_darwin.go
+++ robustio/robustio_darwin.go
@@ -1,11 +1,3 @@
-// SPDX-FileCopyrightText: Copyright © 2025 Ross Smith II <ross@smithii.com>
-// SPDX-License-Identifier: MIT
-
-// SPDX-FileCopyrightText: Copyright 2019 The Go Authors.
-// SPDX-License-Identifier: BSD-3
-
-// Source: https://github.com/golang/go/blob/f15cd63e/src/cmd/internal/robustio/robustio_windows.go
-
 // Copyright 2019 The Go Authors. All rights reserved.
 // Use of this source code is governed by a BSD-style
 // license that can be found in the LICENSE file.
