df5d3b50de209e0f81eb2ee8d26999253ec54b80
Ross Smith II <ross@smithii.com>
2026-07-05 06:06:32 -0700

    fix(test): Change return value in fixPerms to 0o000 for Tinygo/wasm

--- util_test.go
+++ util_test.go
@@ -160,7 +160,7 @@ func init() {
 func fixPerms(perm os.FileMode, isDir bool) os.FileMode {
 	if compat.IsWasip1 {
 		if compat.IsTinygo {
-			return perm600
+			return perm000
 		}
 		if isDir {
 			return perm700
