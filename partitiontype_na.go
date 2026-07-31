190893dc37572fdaadfb3ccae59762ca3d37be91
Ross Smith II <ross@smithii.com>
2026-07-31 13:18:37 -0700

    Fix error instantiation in PartitionType function

--- partitiontype_na.go
+++ partitiontype_na.go
@@ -14,5 +14,5 @@ import (
 // PartitionType returns the filesystem type (e.g., "ext4", "NTFS", "FAT32", etc.)
 // for the disk partition that contains path.
 func PartitionType(_ context.Context, _ string) (string, error) {
-	return "", &compat.NotYetImplementedError("partitionType")
+	return "", &compat.NotYetImplementedError{"partitionType"}
 }
