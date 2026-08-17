f63cf4438b48600f8613e0e6d11c8a28a4c4560a
Ross Smith II <ross@smithii.com>
2026-08-16 23:37:42 -0700

    Enable support for ATime, CTime, and Links

---
+++ supports_big_wasip1.go
@@ -0,0 +1,21 @@
+// SPDX-FileCopyrightText: Copyright © 2026 Ross Smith II <ross@smithii.com>
+// SPDX-License-Identifier: MIT
+
+//go:build wasip1 && !tinygo
+
+package compat
+
+const (
+	supportsATime         = true
+	supportsATimeSetting  = true
+	supportsAtomicReplace = true
+	supportsBTime         = false
+	supportsCTime         = true
+	supportsFstat         = false
+	supportsLinks         = true
+	supportsNice          = false
+	supportsSymlinks      = false
+	supportsUmask         = false
+)
+
+const userIDSource UserIDSourceType = UserIDSourceIsNone
