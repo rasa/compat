1bf89a433cc7193232f55214fbf39cf04cb516a0
Ross Smith II <ross@smithii.com>
2026-08-23 09:34:21 -0700

    Add netbsd && fstat to supports_all.go

--- supports_all.go
+++ supports_all.go
@@ -1,7 +1,7 @@
 // SPDX-FileCopyrightText: Copyright © 2025 Ross Smith II <ross@smithii.com>
 // SPDX-License-Identifier: MIT
 
-//go:build darwin || freebsd || linux || windows
+//go:build darwin || freebsd || linux || (netbsd && fstat) || windows
 
 // The darwin build flag includes ios (which doesn't support Nice())
 
