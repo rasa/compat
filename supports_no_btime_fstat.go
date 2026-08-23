a714d4d191cef5749e55d4a1da8828666e2a9107
Ross Smith II <ross@smithii.com>
2026-08-23 10:11:07 -0700

    Update build constraints for supports_no_btime_fstat.go

--- supports_no_btime_fstat.go
+++ supports_no_btime_fstat.go
@@ -1,7 +1,7 @@
 // SPDX-FileCopyrightText: Copyright © 2025 Ross Smith II <ross@smithii.com>
 // SPDX-License-Identifier: MIT
 
-//go:build (aix || dragonfly || illumos || openbsd || solaris) && !fstat
+//go:build aix || dragonfly || illumos || (openbsd && !fstat) || solaris
 
 package compat
 
