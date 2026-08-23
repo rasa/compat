f4fa823ec97e48f5d29875e9fd66f4cfd5898f48
Ross Smith II <ross@smithii.com>
2026-08-23 10:03:52 -0700

    Modify build constraints in supports_all.go
    Updated build constraints for darwin to exclude ios.

--- supports_all.go
+++ supports_all.go
@@ -1,9 +1,10 @@
 // SPDX-FileCopyrightText: Copyright © 2025 Ross Smith II <ross@smithii.com>
 // SPDX-License-Identifier: MIT
 
-//go:build darwin || freebsd || (linux && !android) || (netbsd && fstat) || windows
+//go:build (darwin && !ios) || freebsd || (linux && !android) || (netbsd && fstat) || windows
 
 // The darwin build flag includes ios (which doesn't support Nice())
+// The linux build flag includes android (which doesn't support BTime())
 
 package compat
 
