b7f1a3b71245f39342af8a1a59be01c71f27ae7a
Ross Smith II <ross@smithii.com>
2026-08-23 09:31:52 -0700

    Rename supports_bsd.go to supports_netbsd.go

--- supports_ netbsd.go
+++ supports_ netbsd.go
@@ -1,7 +1,7 @@
 // SPDX-FileCopyrightText: Copyright © 2025 Ross Smith II <ross@smithii.com>
 // SPDX-License-Identifier: MIT
 
-//go:build !fstat && (freebsd || netbsd)
+//go:build netbsd && !fstat
 
 package compat
 
