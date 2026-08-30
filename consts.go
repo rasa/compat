3a6a9615faa94a4e4e2f709a4ad9d425a225eab3
Ross Smith II <ross@smithii.com>
2026-08-30 12:19:02 -0700

    remove unneeded public constants

--- consts.go
+++ consts.go
@@ -4,7 +4,6 @@
 package compat
 
 import (
-	"io/fs"
 	"os"
 
 	"github.com/rasa/compat/consts"
@@ -60,50 +59,6 @@ const (
 	O_FILE_FLAG_DELETE_ON_CLOSE = consts.O_FILE_FLAG_DELETE_ON_CLOSE
 	// O_FILE_FLAG_NO_RO_ATTR skips setting a file's read-only attribute on Windows.
 	O_FILE_FLAG_NO_RO_ATTR = consts.O_FILE_FLAG_NO_RO_ATTR
-
-	// The following constants are not used by the compat library, but are
-	// provided to make code migration easier.
-
-	// Source: https://github.com/golang/go/blob/ac803b59/src/os/file.go#L81-L89
-
-	// O_RDONLY open the file readonly.
-	O_RDONLY = os.O_RDONLY
-	// O_WRONLY open the file write-only.
-	O_WRONLY = os.O_WRONLY
-	// O_RDWR open the file read-write.
-	O_RDWR = os.O_RDWR
-	// The remaining values may be or'ed in to control behavior.
-
-	// O_APPEND append data to the file when writing.
-	O_APPEND = os.O_APPEND
-	// O_CREATE create a new file if none exists.
-	O_CREATE = os.O_CREATE
-	// O_EXCL used with O_CREATE, file must not exist.
-	O_EXCL = os.O_EXCL
-	// O_SYNC open for synchronous I/O.
-	O_SYNC = os.O_SYNC
-	// O_TRUNC truncate regular writable file when opened.
-	O_TRUNC = os.O_TRUNC
-
-	ModeDir        = fs.ModeDir        // d: is a directory
-	ModeAppend     = fs.ModeAppend     // a: append-only
-	ModeExclusive  = fs.ModeExclusive  // l: exclusive use
-	ModeTemporary  = fs.ModeTemporary  // T: temporary file; Plan 9 only
-	ModeSymlink    = fs.ModeSymlink    // L: symbolic link
-	ModeDevice     = fs.ModeDevice     // D: device file
-	ModeNamedPipe  = fs.ModeNamedPipe  // p: named pipe (FIFO)
-	ModeSocket     = fs.ModeSocket     // S: Unix domain socket
-	ModeSetuid     = fs.ModeSetuid     // u: setuid
-	ModeSetgid     = fs.ModeSetgid     // g: setgid
-	ModeCharDevice = fs.ModeCharDevice // c: Unix character device, when ModeDevice is set
-	ModeSticky     = fs.ModeSticky     // t: sticky
-	ModeIrregular  = fs.ModeIrregular  // ?: non-regular file; nothing else is known about this file
-
-	// ModeType is a mask for the type bits. For regular files, none will be set.
-	ModeType = fs.ModeType
-
-	// ModePerm is a mask for the Unix permission bits, 0o777.
-	ModePerm = fs.ModePerm
 )
 
 // A FileMode represents a file's mode and permission bits.
