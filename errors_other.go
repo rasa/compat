8132abae68f9c521625faafa367484fc094aa856
Ross Smith II <ross@smithii.com>
2026-09-02 16:11:28 -0700

    Simplify normalizeUnsupportedError function
    Removed unnecessary check for unsupported error.

--- errors_other.go
+++ errors_other.go
@@ -11,9 +11,5 @@ import (
 )
 
 func normalizeUnsupportedError(err error) error {
-	if !errors.Is(err, errors.ErrUnsupported) {
-		return fmt.Errorf("%w: %w", errors.ErrUnsupported, err)
-	}
-
 	return err
 }
