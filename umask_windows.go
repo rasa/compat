e1200b2046783996d26dba4ca12cf71c8a52d836
Ross Smith II <ross@smithii.com>
2026-09-01 22:57:53 -0700

    fix: undo umask changes

--- umask_windows.go
+++ umask_windows.go
@@ -1,7 +1,7 @@
 // SPDX-FileCopyrightText: Copyright © 2025 Ross Smith II <ross@smithii.com>
 // SPDX-License-Identifier: MIT
 
-//go:build !(js || unix || (wasip1 && !tinygo))
+//go:build windows
 
 package compat
 
